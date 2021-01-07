d778f9d6e6fa39c77a7441468d35a663952c46a3
Yi-Fan Peng 彭羿凡 <ponpon010548@gmail.com>
2021-01-07 08:36:00 +0800

    Update LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 struct Node{
-	int data;
-	struct Node *next;
+    int data;
+    struct Node *next;
 };
 
 struct pointers{
-	struct Node *head;
-	struct Node *tail;
+    struct Node *head;
+    struct Node *tail;
 };
 
 void push(struct pointers *ptr, int data);
@@ -17,58 +17,56 @@ void reverseList(struct pointers *ptr);
 void delete(struct pointers *ptr, int data); //deletes the first occurrence of key in linked list
 
 int main(void) {
-	struct pointers *ptr = (struct pointers *)malloc(sizeof(struct pointers));
-	ptr -> head = NULL;
-	ptr -> tail = NULL;
-	for(int i = 0; i < 10; i++)
-		push(ptr, i);
-	printList(ptr);
-	reverseList(ptr);
-	printList(ptr);
-	delete(ptr, 5);
-	printList(ptr);
-	return 0;
+    struct pointers *ptr = (struct pointers *)malloc(sizeof(struct pointers));
+    ptr -> head = NULL;
+    ptr -> tail = NULL;
+    for(int i = 0; i < 10; i++)
+	push(ptr, i);
+    printList(ptr);
+    reverseList(ptr);
+    printList(ptr);
+    delete(ptr, 5);
+    printList(ptr);
+    return 0;
 }
 
 
 void push(struct pointers *ptr, int data){
-	struct Node *new = (struct Node *)malloc(sizeof(struct Node));
-	if(!ptr -> head)
-		ptr -> head = new;
-	new -> data = data;
-	new -> next = NULL;
-	if(ptr -> tail)
-		ptr -> tail -> next = new;
-	ptr -> tail = new;
+    struct Node *new = (struct Node *)malloc(sizeof(struct Node));
+    if(!ptr -> head)
+	ptr -> head = new;
+    new -> data = data;
+    new -> next = NULL;
+    if(ptr -> tail)
+	ptr -> tail -> next = new;
+    ptr -> tail = new;
 }
 
 void delete(struct pointers *ptr, int data){
-	struct Node *del, *pre;
-	for(del = ptr -> head; del -> data != data; del = del -> next)
-		pre = del;
-	pre -> next = del -> next;
-	del -> next = NULL;
-	free(del);
+    struct Node *del, *pre;
+    for(del = ptr -> head; del -> data != data; del = del -> next)
+	pre = del;
+    pre -> next = del -> next;
+    del -> next = NULL;
+    free(del);
 }
 
 void printList(struct pointers *ptr){
-	struct Node *p;
-	for(p = ptr -> head; p != NULL; p = p -> next)
-		printf("%d -> ", p -> data);
-	printf("NULL\n");
+    struct Node *p;
+    for(p = ptr -> head; p != NULL; p = p -> next)
+        printf("%d -> ", p -> data);
+    printf("NULL\n");
 }
 
 void reverseList(struct pointers *ptr){
-	struct Node *curr, *pre = NULL, *nextNode = NULL;
-	curr = ptr -> head;
-	while(curr){
-		nextNode = curr -> next;
-		curr -> next = pre;
-		pre = curr;
-		curr = nextNode;
-	}
-	ptr -> tail = ptr -> head;
-	ptr -> head = pre;
+    struct Node *curr, *pre = NULL, *nextNode = NULL;
+    curr = ptr -> head;
+    while(curr){
+	nextNode = curr -> next;
+	curr -> next = pre;
+	pre = curr;
+	curr = nextNode;
+    }
+    ptr -> tail = ptr -> head;
+    ptr -> head = pre;
 }
-
-
